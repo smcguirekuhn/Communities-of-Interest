1662adb5313ec2e9df33b08d1cb1cb4854c8ea76
Spencer Kuhn <smcguirekuhn@gmail.com>
2026-09-09 22:27:55 -0400

    Iteration, frequency, and contextual accuracy tables, new format of web comment data

--- Functions/extractCommentRequests.R
+++ Functions/extractCommentRequests.R
@@ -1,5 +1,5 @@
 
-extractLocationRelationships <- function(
+extractCommentRequests <- function(
     comment,
     locationNames,
     districtTypes = c(
@@ -21,7 +21,7 @@ extractLocationRelationships <- function(
   chat <- ellmer::chat_openrouter(model = model)
 
   # extract comment location relationships ----
-  relationships <- chat$chat_structured(
+  requests <- chat$chat_structured(
     comment,
     type = ellmer::type_array(
       description = ellmer::interpolate(
@@ -31,33 +31,33 @@ extractLocationRelationships <- function(
         Return any pairwise location relationships requested by the commenter.
         Interpret the commenter's request, complaint, or concern.
         Do not treat the composition of an existing or proposed district as a desired relationship 
-        unless the commenter explicitly endorses or rejects that composition.
+        unless the commenter explicitly endorses or rejects that composition.\n\n
         
-        The following are examples of a grouped pairwise relationship between 'Springfield' and 'Washington County':
-        'Vote no on the proposed map, which separates Springfield from Washington County.',
-        'Springfield and Washington County have been separated. This is a clear effort to Gerrymander our community.',
-        'Springfield and Washington County share similar economic interests.',
-        'Why is Springfield in a different district than Washington County?',
-        'Separating Springfield from Washington County is ridiculous.',
-        'Please Keep Springfield together with Washington County.',
-        'Don't split Springfield from Washington County.', and
-        'Springfield should be reunited with Washington County.'.
+        The following are examples of a 'grouped' pairwise relationship between 'Springfield' and 'Washington County':\n
+        - 'Vote no on the proposed map, which separates Springfield from Washington County.'\n
+        - 'Springfield and Washington County have been separated. This is a clear effort to Gerrymander our community.'\n
+        - 'Springfield and Washington County share similar economic interests.'\n
+        - 'Why is Springfield in a different district than Washington County?'\n
+        - 'Separating Springfield from Washington County is ridiculous.'\n
+        - 'Please Keep Springfield together with Washington County.'\n
+        - 'Don't split Springfield from Washington County.'\n
+        - 'Springfield should be reunited with Washington County.'\n\n
         
-        The following are examples of a separated pairwise relationship between 'Springfield' and 'Washington County':
-        'Vote no on the proposed map, which combines Springfield with Washington County.',
-        'Springfield and Washington County have been combined. This is a clear effort to Gerrymander our community.',
-        'Springfield and Washington County do not share similar economic interests.',
-        'Why is Springfield in the same district as Washington County?',
-        'Combining Springfield and Washington County is ridiculous',
-        'Please Keep Springfield separated from Washington County.',
-        'Don't group Springfield with Washington County.', and
-        'Springfield should be kept apart from Washington County.'."
+        The following are examples of a 'separated' pairwise relationship between 'Springfield' and 'Washington County':\n
+        - 'Vote no on the proposed map, which combines Springfield with Washington County.'\n
+        - 'Springfield and Washington County have been combined. This is a clear effort to Gerrymander our community.'\n
+        - 'Springfield and Washington County do not share similar economic interests.'\n
+        - 'Why is Springfield in the same district as Washington County?'\n
+        - 'Combining Springfield and Washington County is ridiculous.'\n
+        - 'Please Keep Springfield separated from Washington County.'\n
+        - 'Don't group Springfield with Washington County.'\n
+        - 'Springfield should be kept apart from Washington County.'"
       ),
       items = ellmer::type_object(
         Location1 = ellmer::type_enum(values = locationNames),
         Location2 = ellmer::type_enum(values = locationNames),
         Relationship = ellmer::type_enum(
-          description = ellmer::interpolate("Return the nature of the relationship between the two locations"),
+          description = "Return the nature of the requested relationship between locations.",
           values = c("grouped", "separated", "unclear")),
         DistrictType = ellmer::type_enum(
           description = ellmer::interpolate(
@@ -73,5 +73,5 @@ extractLocationRelationships <- function(
   )
 
   # return comment location relationships ----
-  return(relationships)
+  return(requests)
 }
